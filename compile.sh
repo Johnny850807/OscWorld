a6987b428d7688089b98be1d6ca66ac13970ec80
johnny850807 <johnny850807@gmail.com>
2020-06-22 15:59:53 +0800

    :whale: docker-compile for maven

---
+++ compile.sh
@@ -0,0 +1,2 @@
+sudo DOCKER_BUILDKIT=1 docker build . -t maven-compile -f Dockerfile-compile
+sudo docker run --rm  -v $(pwd):/usr/src/app  -v ~/.m2:/.m2 -v $(pwd)/target:/usr/src/app/target maven-compile
